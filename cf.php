0bf5837d427d9cbf3fbc96eef05917687c37e885
Melvin <melvinmathew0102@gmail.com>
2019-04-05 16:11:36 +0530

    Updated

--- cf.php
+++ cf.php
@@ -1,28 +1,12 @@
 <?php
-if(isset($_POST["sub"])){
-// Checking For Blank Fields..
-if($_POST["vname"]==""||$_POST["vemail"]==""||$_POST["msg"]==""){
-echo "Fill All Fields..";
-}else{
-// Check if the "Sender's Email" input field is filled out
-$email=$_POST['vemail'];
-// Sanitize E-mail Address
-$email =filter_var($email, FILTER_SANITIZE_EMAIL);
-// Validate E-mail Address
-$email= filter_var($email, FILTER_VALIDATE_EMAIL);
-if (!$email){
-echo "Invalid Sender's Email";
-}
-else{
-$subject = "Subject";
-$message = $_POST['msg'];
-$headers = 'From:'. $email2 . "rn"; // Sender's Email
-$headers .= 'Cc:'. $email2 . "rn"; // Carbon copy to Sender
-// Message lines should not exceed 70 characters (PHP rule), so wrap it
-$message = wordwrap($message, 70);
-// Send Mail By PHP Mail Function
-mail("info@krayontraders.com", $subject, $message, $headers);
-echo "Your mail has been sent successfuly ! Thank you for your feedback";
-}
-}
+if (isset($_POST['sub'])) {
+    $name=$_POST['vname'];
+    $sub="sub";
+    $mailFrom=$_POST['vemail'];
+    $message=$_POST['msg'];
+    $mailTo="info@krayontraders.com";
+    $headers="From:".$mailFrom;
+    $txt="You have recieved an email from ".$name."\n\n".$message;
+    mail($mailTo,$sub,$txt,$headers);
+    header("Location: index.php?mailsend");
 }
